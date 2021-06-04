0ac2cd72591f5778a215e2d1215b378fc899eb8d
anjali180 <anjaliarlod@gmail.com>
2021-06-04 12:07:56 +0530

    add solution

--- wallis.c
+++ wallis.c
@@ -23,14 +23,7 @@ int main(void) {
     }
   }
 }
-#include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
-float wallis_pi(int);
-
-float wallis_pi(int n)
+    float wallis_pi(int n)
 {
 float k=1.0;
    int i;
@@ -41,26 +34,4 @@ float k=1.0;
 
 
   return (k*2);
-}
-    
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    //printf("wallis called %f %f\n",M_PI,pi) ;
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      //printf("wallis g %f\n",pi) ;
-  printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-          abort();
-    }
-  }
-
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
-    }
-  }
-  
-}
+     }
